cc4964e409400e9c5a433ef4544b13b85878fd7e
oizquierdo <Oizquierdo@voxelgroup.net>
2021-12-10 20:07:08 +0100

    Initial commit

---
+++ PointOfSale.Test/BarcodeTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace PointOfSale.Test
+{
+    public class BarcodeTests
+    {
+        [Fact]
+        public void Barcode12345display725dollars()
+        {
+            
+        }
+    }
+}
---
+++ PointsOfSale/Barcode.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PointsOfSale
+{
+    public class Barcode
+    {
+        
+    }
+}
