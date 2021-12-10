a6269c603f55d6a5ca54f4ae3049bc76998a4611
oizquierdo <Oizquierdo@voxelgroup.net>
2021-12-10 20:11:31 +0100

    test: Barcode12345display7dot25dollars

--- PointOfSale.Test/BarcodeTests.cs
+++ PointOfSale.Test/BarcodeTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using FluentAssertions;
+using PointsOfSale;
 
 namespace PointOfSale.Test
 {
@@ -9,7 +10,11 @@ namespace PointOfSale.Test
         [Fact]
         public void Barcode12345display725dollars()
         {
+            Barcode barcode = new Barcode("12345");
+
+            barcode.Scan();
             
+            barcode.Should().Be("$7.25");
         }
     }
 }
--- PointsOfSale/Barcode.cs
+++ PointsOfSale/Barcode.cs
@@ -4,6 +4,14 @@ namespace PointsOfSale
 {
     public class Barcode
     {
+        private readonly string _barcode;
+
+        public Barcode(string barcode)
+            => _barcode = barcode;
         
+        public void Scan()
+        {
+            
+        }
     }
 }
